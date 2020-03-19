43df810c7e4a6c53bfe98b79fb0a09b48eb9a373
MuttakinHasib <hasibmolla28@gmail.com>
2020-03-19 21:44:39 +0600

    update

--- js/covid.js
+++ js/covid.js
@@ -5,11 +5,11 @@ class COVID {
 
   async corona() {
     const res = await fetch("https://coronavirus-19-api.herokuapp.com/all");
-    const resCountry = await fetch(
-      `https://covid19.mathdro.id/api/countries/${this.city}`
-    );
+    // const resCountry = await fetch(
+    //   `https://covid19.mathdro.id/api/countries/${this.city}`
+    // );
     const global = await res.json();
-    const country = await resCountry.json();
+    // const country = await resCountry.json();
     return {
       global,
       country
